1bdc310d17b5fdfdf0c8d61c156096c85c543b57
Hikarunathilake <hikarunathilake@students.nsbm.ac.lk>
2026-08-22 20:43:09 +0530

    ss

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
+
+laod_dotenv()
+
+inngest_client = inngest.Client(
+app_id="rag_app",
+logging=logging.getLogger("uvicorn")
+is_production=False,
+serializer=inngest.PydanticSerializer()
+
+)
+
+app
